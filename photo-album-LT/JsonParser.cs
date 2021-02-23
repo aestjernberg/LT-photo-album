253dc176ff415905aeb1eb81685443f43eadd8e4
aestjernberg <andrewstjernberg@yahoo.com>
2021-02-22 18:23:00 -0600

    First commit

---
+++ photo-album-LT/JsonParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace photo_album_LT
+{
+    class JsonParser
+    {
+        private string url;
+
+        public void setURL(string albumID)
+        {
+            while (!Regex.Match(albumID, "^[0-9]+$").Success || Convert.ToInt32(albumID) < 1 || Convert.ToInt32(albumID) > 100)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a valid album ID: ");
+                albumID = Console.ReadLine();
+            }
+
+            url = "https://jsonplaceholder.typicode.com/photos?albumId=" + albumID;
+        }
+
+        public string getURL()
+        {
+            return url;
+        }
+
+        public JArray getJsonData()
+        {
+            string json = (new WebClient()).DownloadString(url);
+            return JArray.Parse(json);
+        }
+    }
+}
---
+++ photo-album-LT/Program.cs
@@ -0,0 +1,72 @@
+﻿using ConsoleTables;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+
+namespace photo_album_LT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] inputArray;
+            string albumID;
+            string flag;
+            JsonParser jp = new JsonParser();
+            Console.WriteLine("Instructions:");
+            Console.WriteLine("1. Enter the desired album ID (1-100)");
+            input = Console.ReadLine();
+
+            while (input != "Q" && input != "q")
+            {
+                inputArray = input.Split(" ");
+                albumID = inputArray[0];
+                flag = inputArray[1];
+
+                if (flag == "a")
+                {
+                    printInfoByPhotoID(jp, albumID);
+                }
+                else if (flag == "b")
+                {
+                    printInfoByTitle(jp, albumID);
+                }
+
+                Console.WriteLine("Please enter your desired command:");
+                input = Console.ReadLine();
+            }
+        }
+
+        public static void printInfoByPhotoID(JsonParser jp, string albumID)
+        {
+            jp.setURL(albumID);
+            Console.Write("\r\n");
+
+            var table = new ConsoleTable("PHOTO ID", "TITLE");
+            foreach (JObject item in jp.getJsonData())
+            {
+                int id = (int)item.GetValue("id");
+                string title = item.GetValue("title").ToString();
+                table.AddRow(id, title);
+            }
+            table.Write(Format.Minimal);
+        }
+
+        public static void printInfoByTitle(JsonParser jp, string albumID)
+        {
+            jp.setURL(albumID);
+            JArray array = jp.getJsonData();
+            JArray sorted = new JArray(array.OrderBy(obj => (string)obj["title"]));
+
+            var table = new ConsoleTable("PHOTO ID", "TITLE");
+            foreach (JObject item in sorted)
+            {
+                int id = (int)item.GetValue("id");
+                string title = item.GetValue("title").ToString();
+                table.AddRow(id, title);
+            }
+            table.Write(Format.Minimal);
+        }
+    }
+}
